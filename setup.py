6f0dd84240bd3d1916e9042527323d1185130458
PlanB <city7310@naver.com>
2017-10-29 21:30:53 +0900

    [DONE] setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='flask- restful-autoroute',
+      version='0.1',
+      url='https://github.com/JoMingyu/Flask-restful-autoroute',
+      license='MIT',
+      author='PlanB',
+      author_email='city7310@naver.com',
+      description='Blueprint based auto routing library for flask-restful',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['flask_restful_autoroute'])
